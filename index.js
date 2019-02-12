941ddbfd5a1073aa2b065602bda739acd0f0a043
Kyle Graden <kylegraden3@gmail.com>
2019-02-12 00:27:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ function happyHolidays() {
 }
 
  function happyHolidaysTo(name) {
-   console.log('Happy holidays, ${name}')
+   return 'Happy holidays, ${name}'
 }
 
  function happyHolidayTo(holiday, name) {
-  console.log('Happy ${holiday}, ${name}!')
+  return 'Happy ${holiday}, ${name}!'
  }
 
 
  function holidayCountdown(days, mother) {
-   console.log('It\'s ${days} days until ${holiday}!')
+   return 'It\'s ${days} days until ${holiday}!')
  }
