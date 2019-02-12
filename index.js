7c83c5b14e1fff2b226fc231881c52a7d587bf6f
Kyle Graden <kylegraden3@gmail.com>
2019-02-12 00:17:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ function happyHolidays() {
 happyHolidaysTo('you')
 
  function happyHolidayTo(holiday, name) {
-  return "Happy ${holiday}, " + ${name}
+  console.log('Happy ${holiday}, ${name}!')
  }
- 
+
 happyHolidayTo('Independence Day','you')
 
  function holidayCountdown(days, holiday) {
-   return "It\'s ${days} days until ${holiday}!"
+   console.log('It\'s ${days} days until ${holiday}!')
  }
 
  holidayCountdown(20, "Mother\'s Day")
